c2ad0a9a3c7c31689e37c4f6f99ff78522009d73
jacobdlewis <jacobdlewis@gmail.com>
2015-05-11 14:26:05 -0500

    added integration test for adding a course to the Wish List.

--- test/integration/test_adding_a_wish_list_course.rb
+++ test/integration/test_adding_a_wish_list_course.rb
@@ -89,4 +89,39 @@ EOS
     assert_equal expected_output, shell_output
   end
 
+  def test_adding_a_course
+    shell_output = ""
+    expected_output = ""
+    IO.popen('./disc_golf_course_tracker', 'r+') do |pipe|
+      expected_output = <<EOS
+1. Add a course to your Wish List
+2. View your course Wish List
+3. Remove a course from your Wish List
+4. View your played courses
+5. Review a played course
+6. Import Data
+7. Exit
+EOS
+      pipe.puts "1"
+      expected_output << "What is the name of the course?\n"
+      pipe.puts "Seven Oaks Park"
+      expected_output << "In what city is the course located?\n"
+      pipe.puts "Nashville"
+      expected_output << "In what state is the course located?\n"
+      pipe.puts "TN"
+      expected_output << "What is the street address for the course?\n"
+      pipe.puts "3457 School Ln"
+      expected_output << "What is the ZIP code for the course?\n"
+      pipe.puts "37217"
+      expected_output << "How many holes does the course have?\n"
+      pipe.puts "18"
+      expected_output << "Is the course paid or free?\n"
+      pipe.puts "free"
+      expected_output << "Thank you. Seven Oaks Park has been added to your course Wish List.\n"
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    assert_equal expected_output, shell_output
+  end
+
 end
