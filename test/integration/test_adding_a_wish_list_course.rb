e5e65fb223169ab7db44a31334cc90b7bce40795
jacobdlewis <jacobdlewis@gmail.com>
2015-05-11 13:38:40 -0500

    added initial integration tests

--- test/integration/test_adding_a_wish_list_course.rb
+++ test/integration/test_adding_a_wish_list_course.rb
@@ -42,22 +42,17 @@ class AddNewCourseTest < Minitest::Test
   def test_shows_menu_on_load
     shell_output = ""
     expected_output = ""
-    IO.popen('./disc_golf_course_tracker') do |pipe|
+    IO.popen('./disc_golf_course_tracker', 'r+') do |pipe|
       expected_output = <<EOS
-
-Hello. Welcome to Disc Golf Course Tracker!
-
-What would you like to do?
-
-1. Add a course to you Wish List
+1. Add a course to your Wish List
 2. View your course Wish List
 3. Remove a course from your Wish List
 4. View your played courses
 5. Review a played course
 6. Import Data
 7. Exit
-
 EOS
+    pipe.close_write
     shell_output = pipe.read
     end
   assert_equal expected_output, shell_output
@@ -74,6 +69,24 @@ EOS
   end
 
   def test_exiting_program
+    shell_output = ""
+    expected_output = ""
+    IO.popen('./disc_golf_course_tracker', 'r+') do |pipe|
+      expected_output = <<EOS
+1. Add a course to your Wish List
+2. View your course Wish List
+3. Remove a course from your Wish List
+4. View your played courses
+5. Review a played course
+6. Import Data
+7. Exit
+EOS
+      pipe.puts "7"
+      expected_output << "Goodbye!\n"
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    assert_equal expected_output, shell_output
   end
 
 end
